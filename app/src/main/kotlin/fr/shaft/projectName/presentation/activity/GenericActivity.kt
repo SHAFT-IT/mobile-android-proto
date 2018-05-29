771671027f74bc7f41084fcbf159efb562dcfa8e
randrianarivo-mandimby <mandimby.randrianarivo@shaft-it.com>
2018-05-29 15:14:31 +0200

    init repo

---
+++ app/src/main/kotlin/fr/shaft/projectName/presentation/activity/GenericActivity.kt
@@ -0,0 +1,9 @@
+package fr.shaft.projectName.presentation.activity
+
+import android.support.v7.app.AppCompatActivity
+
+abstract class GenericActivity : AppCompatActivity(){
+
+
+
+}
---
+++ app/src/main/kotlin/fr/shaft/projectName/presentation/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package fr.shaft.projectName.presentation.activity
+
+import fr.shaft.projectName.R
+import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.EActivity
+
+@EActivity(R.layout.activity_main)
+open class MainActivity : GenericActivity() {
+
+    @AfterViews
+    fun initialize() {
+
+
+
+    }
+
+}
